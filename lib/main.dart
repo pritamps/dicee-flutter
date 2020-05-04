cd50705283abc81e5026bbb73daeb7c4bbf37be6
Pritam Sukumar <pritam@avanti.in>
2020-05-04 18:18:15 +0530

    Oooh

--- lib/main.dart
+++ lib/main.dart
@@ -58,6 +58,6 @@ class _DicePageState extends State<DicePage> {
     setState(() {
       leftDiceNumber = Random().nextInt(6) + 1;
       rightDiceNumber = Random().nextInt(6) + 1;
-    })
+    });
   }
 }
